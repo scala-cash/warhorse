44b1e029fd593c436814d98c3c62a778c9405826
sken <sken77@pm.me>
2020-04-28 21:55:11 -0700

    fix compiling issue

--- core/src/main/scala/scash/warhorse/core/crypto/Schnorr.scala
+++ core/src/main/scala/scash/warhorse/core/crypto/Schnorr.scala
@@ -1,12 +1,10 @@
 package scash.warhorse.core.crypto
 
-/*
 import scash.warhorse.Result
 import scodec.bits.ByteVector
 
-class Schnorr(implicit e: ECCurve[Secp256k1]) extends Signer[Schnorr] {
+class Schnorr extends Signer[Schnorr] {
   def sign(msg: ByteVector, privkey: PrivateKey): Result[Signature[Schnorr]] = Predef.???
 
   def verify(msg: ByteVector, sig: Signature[Schnorr], pubkey: PublicKey): Boolean = Predef.???
 }
- */
